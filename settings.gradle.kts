8efb032cdeaa7c81578c06b46b79b50252370098
daniel <daniel.ramirez.s@tecsup.edu.pe>
2024-06-26 18:27:27 -0500

    movil terminado

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ dependencyResolutionManagement {
             credentials {
                 username = "paypal_sgerritz"
                 password =
-                    "AKCp8jQ8tAahqpT5JjZ4FRP2mW7GMoFZ674kGqHmupTesKeAY2G8NcmPKLuTxTGkKjDLRzDUQ"
+                    "clave"
             }
 
         }
